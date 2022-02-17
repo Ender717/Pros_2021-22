4e8d677d1c87a69d364e775a985367d3276d6fc7
Ender717 <nathan.sandvig@gmail.com>
2022-02-17 15:43:15 -0600

    Added a basic auton controller header file - Nathan S, 2-17-22

---
+++ include/AutonController.h
@@ -0,0 +1,30 @@
+// Inclusion guard
+#ifndef AUTON_CONTROLLER_H
+#define AUTON_CONTROLLER_H
+
+// Included libraries
+#include "./main.h"
+
+//-----------------------------------------------------------------------------
+// This class manages the autonomous control system
+// v1: Created the class - Nathan S, 2-17-22
+//-----------------------------------------------------------------------------
+class AutonController
+{
+private:
+    //-------------------------------------------------------------------------
+    // Private data members
+    // NUM_TASKS: The number of tasks the system runs concurrently
+    //-------------------------------------------------------------------------
+    const int NUM_TASKS = 6;
+    pros::Task[] taskList;
+
+public:
+    //-------------------------------------------------------------------------
+    // Default constructor for the auton controller class
+    // v1: Created the constructor - Nathan S, 2-17-22
+    //-------------------------------------------------------------------------
+    AutonController();
+};
+
+#endif
