12f108f2ee0c40d4c0436ddfe31b08dfbf7cc292
Ender717 <nathan.sandvig@gmail.com>
2022-03-11 19:10:54 -0600

    d

--- src/autons.cpp
+++ src/autons.cpp
@@ -11,32 +11,64 @@ namespace Autons
     // Functions
     void ProgrammingSkills1()
     {
-        AutonController control(-60.0, 45.0, 90.0);
+        AutonController control(-60.0, 46.0, 90.0);
 
         // Grab the goal
-        control.DoPositionTask(-60.0, 43.0, 127.0, 110.0, true, true, false);
-        pros::delay(250);
+        control.DoPositionTask(-60.0, 44.0, 127.0, 110.0, true, true, false);
+        pros::delay(50);
 
-        control.DoPositionTask(-60.0, 50.0, 127.0, 110.0, true, false, false);
-        pros::delay(250);
+        control.DoPositionTask(-60.0, 51.0, 127.0, 110.0, true, false, false);
+        pros::delay(50);
 
-        control.DoTurnTask(-10.0, 127.0, 110.0, false, false, false);
-        pros::delay(250);
+        control.DoTurnTask(-5.0, 127.0, 110.0, false, false, false);
+        pros::delay(50);
 
-        control.DoPositionTask(-30.0, 39.0, 127.0, 110.0, false, false, true);
-        pros::delay(250);
+        control.DoPositionTask(45.0, 40.0, 127.0, 110.0, false, false, true);
+        pros::delay(50);
+
+        control.DoPositionTask(40.0, 40.0, 127.0, 110.0, false, false, true);
+        pros::delay(50);
+
+        control.DoTurnTask(30.0, 127.0, 110.0, false, false, false);
+        pros::delay(50);
+
+        control.DoPositionTask(60.0, 64.0, 127.0, 110.0, false, true, true);
+        pros::delay(50);
+
+        control.DoTurnTask(0.0, 127.0, 110.0, false, false, false);
+        pros::delay(50);
+
+        control.DoPositionTask(-40.0, 64.0, 127.0, 110.0, false, false, true);
+        pros::delay(50);
+
+        /*
+        control.DoPositionTask(-30.0, 40.0, 127.0, 70.0, false, false, true);
+        pros::delay(50);
 
         control.DoTurnTask(0.0, 127.0, 0.0, false, false, false);
-        pros::delay(250);
+        pros::delay(50);
 
-        control.DoPositionTask(-15.0, 39.0, 127.0, -17.0, false, false, false);
-        pros::delay(250);
+        control.DoPositionTask(-15.0, 46.0, 127.0, -20.0, false, false, false);
+        pros::delay(50);
 
-        control.DoPositionTask(-10.0, 39.0, 127.0, -17.0, true, false, false);
-        pros::delay(250);
+        control.DoTurnTask(0.0, 127.0, -17.0, false, false, false);
+        pros::delay(50);
 
-        control.DoPositionTask(0.0, 39.0, 127.0, 110.0, true, false, false);
-        pros::delay(250);
+        control.DoPositionTask(-9.0, 46.0, 127.0, -20.0, true, false, false);
+        pros::delay(50);
+
+        control.DoPositionTask(0.0, 32.0, 127.0, 110.0, true, false, false);
+        pros::delay(50);
+
+        control.DoPositionTask(24.0, 8.0, 127.0, 110.0, true, false, false);
+        pros::delay(50);
+
+        control.DoTurnTask(0.0, 127.0, 110.0, true, false, false);
+        pros::delay(50);
+
+        control.DoPositionTask(40.0, 0.0, 127.0, 110.0, true, false, false);
+        pros::delay(50);
+        */
     }
 
     void ProgrammingSkills2()
